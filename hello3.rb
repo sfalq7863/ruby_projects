35254aa651f7a2588743a600efcc3500bf273af6
”sfalq7863” <sutefani7863@yahoo.co.jp>
2019-10-15 12:32:58 +0000

    rubyの課題3

---
+++ hello3.rb
@@ -0,0 +1,49 @@
+n = 3
+if n == 3
+  puts "nは3です"
+end
+if n = 3 || n = 4
+  puts "3,4のどちらかです"
+end
+n = 3
+if n == 3
+  puts "nは3です"
+  elsif n == 4
+  puts "nは4です"
+end
+n = 4
+if n == 3
+  puts "nは3です"
+  elsif n == 4
+  puts "nは4です"
+end
+for hello in ["こんにちは","こんにちは","こんにちは","こんにちは","こんにちは"] do
+  p hello 
+end
+[1,2,3,4,5].each do |num|
+  p num
+end
+n = 2
+if n == 2
+  puts "2です"
+  elsif n == 3
+  puts "3です"
+else
+  puts "それ以外です"
+end
+n = 3
+if n == 2
+  puts "2です"
+  elsif n == 3
+  puts "3です"
+else
+  puts "それ以外です"
+end
+n = 1
+if n == 2
+  puts "2です"
+  elsif n == 3
+  puts "3です"
+else
+  puts "それ以外です"
+end
